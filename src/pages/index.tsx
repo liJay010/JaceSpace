eca1d8745ffc4b966e6aeb7dd9fd5a5db8bf99e8
LiJay010 <571303526@qq.com>
2023-02-17 19:39:21 +0800

    mod aaa

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
               <div className={styles.buttons}>
                   <Link
                       className="button button--secondary button--lg"
-                      to="/docs/deep_learning/基础知识/深度学习基础知识目录">
+                      to="/docs/deep_learning/基础知识/aaa">
                       ENTER
                   </Link>
               </div>
